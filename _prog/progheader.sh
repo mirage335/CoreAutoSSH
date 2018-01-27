261176c04c2f6c822dfd968617fb26530988ece7
mirage335 <spamfreemirage335@gmail.com>
2018-01-27 03:11:47 -0500

    Modern ubiquitous bash compile script.

---
+++ _prog/progheader.sh
@@ -0,0 +1,4 @@
+#Core Auto SSH v1.0
+
+#http://creativecommons.org/publicdomain/zero/1.0/
+#To the extent possible under law, mirage335 has waived all copyright and related or neighboring rights to Core Auto SSH v1.0, excepting any credential files. This work is published from: United States.
